a3b2ab642f826eb31187482f1d400e6665c80030
MuriloPrudencio <90528471+MuriloPrudencio@users.noreply.github.com>
2022-02-28 21:16:00 -0300

    Finalização Home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
 
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+
 export default function Home() {
     return (
       <>
-      <h1>My DRE</h1>
+      <div className={styles.home_container}>
+        <div className={styles.home_title}>
+          <h1>Seja Bem vido</h1>
+          <p>Faça sua DRE</p>
+          <button className={styles.home_btn}><Link href="/">DRE</Link></button>
+        </div>
+        <div>
+         <img className={styles.home_imagem}
+         src="/img/BackHome.jpg"
+         alt="contabilidade"
+         />
+        </div>       
+      </div>
+      <div className={styles.home_text}>
+          <h2>My DRE</h2>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil nam, dolorem aspernatur numquam quisquam cum excepturi
+           praesentium id esse asperiores inventore accusantium quod ratione voluptatum odit dolorum consequatur nostrum autem.
+         </p>
+        </div>
       </>
     )
 }
